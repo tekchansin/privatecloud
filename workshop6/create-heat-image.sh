4673b0946cef875bc8ccf49843e3fed1a4e1a7eb
sawangpong muadphet <sawangpongm@gmail.com>
2016-02-18 12:12:49 +0700

    commit

--- workshop6/create-heat-image.sh
+++ workshop6/create-heat-image.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-yum install epel-release
+
 yum install qemu-img
 yum install python-pip git
-pip install git+git://git.openstack.org/openstack/dib-utils.git
+
 git clone https://git.openstack.org/openstack/diskimage-builder.git
+git clone https://git.openstack.org/openstack/tripleo-image-elements.git
+git clone https://git.openstack.org/openstack/heat-templates.git
+git clone https://git.openstack.org/openstack/dib-utils.git
+export PATH="${PWD}/dib-utils/bin:$PATH"
+						
 export ELEMENTS_PATH=tripleo-image-elements/elements:heat-templates/hot/software-config/elements
 export BASE_ELEMENTS="centos7 selinux-permissive"
 export AGENT_ELEMENTS="os-collect-config os-refresh-config os-apply-config"
 export DEPLOYMENT_BASE_ELEMENTS="heat-config heat-config-script"
-export DEPLOYMENT_TOOL=""
 export IMAGE_NAME=software-deployment-image
 diskimage-builder/bin/disk-image-create vm $BASE_ELEMENTS $AGENT_ELEMENTS $DEPLOYMENT_BASE_ELEMENTS $DEPLOYMENT_TOOL -o $IMAGE_NAME.qcow2
-glance image-create --name CentOS-7-x86_64-heat --disk-format qcow2 --container-format bare --file software-deployment-image.qcow2
-
